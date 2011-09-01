913eedf77ab1be7e148f65c39d1db6aae0a97499
Administrator <Administrator@john-PC>
2011-09-01 11:10:06 +0800

    add model seller

---
+++ app/models/Seller.java
@@ -0,0 +1,43 @@
+/**
+ *
+ */
+package models;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import play.data.validation.MaxSize;
+import play.data.validation.Required;
+import play.db.jpa.GenericModel;
+
+@Entity
+public class Seller extends GenericModel {
+
+	@Id
+	@GenericGenerator(name = "generator", strategy = "foreign", parameters = { @Parameter(name = "property", value = "user") })
+	@GeneratedValue(generator = "generator")	
+	public Long userid;
+
+	@OneToOne(cascade = CascadeType.PERSIST)
+	@PrimaryKeyJoinColumn
+	public User user;
+
+	@Required
+	@MaxSize(value = 256)
+	public String companyname;
+	@Required
+	@MaxSize(value = 256)
+	public String companyaddress;
+
+	public String bank;
+	public String banknum;
+
+}
